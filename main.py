8e9571a63caccb51c2e79d23e96fe1a490d423db
Theophile Sautory <theophile.sautory15@imperial.ac.uk>
2020-01-20 12:48:34 +0000

    yes

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-## Main code
-a = 5 + 3 - 2
+## Main code for GMM-EM
+
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+import numpy as np
+from sklearn.datasets.samples_generator import make_blobs
+
+def data_creation():
+    X, y_true = make_blobs(n_samples=400, centers=4,
+                       cluster_std=0.60, random_state=0)
+    return X[:, ::-1]
+
+class gaussians():
+
+    def __init__(self, mean, covariance):
+        self.mean = mean
+        self.covariance = covariance
+
+        
